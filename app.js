04b61e18077acfc9ad3cf82c25f8cde39c61fd6a
Tiagomt <tiago-tmt@hotmail.com>
2022-04-15 17:49:36 -0300

    Lógica básica concluida

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+//pegando displays
 const computerChoiceDisplay = document.getElementById('computer-choice')
-
 const userChoiceDisplay = document.getElementById('user-choice')
-
 const resultDisplay = document.getElementById('result')
 
 //pegando todas as tags button
 const possibleChoices = document.querySelectorAll("button")
 
+//variaveis globais
 let userChoice
+let computerChoice
+let result
 
 //para cada tag button, adicionar um evento
 possibleChoices.forEach(button => button.addEventListener("click", (e) => {
@@ -16,4 +18,51 @@ possibleChoices.forEach(button => button.addEventListener("click", (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
 
+    generateComputerChoice()
+    getResult()
+
 }))
+
+function generateComputerChoice() {
+
+    //sorteando um número aleatorio com a quantidade de possibilidades(botoes), no caso 3(0-2)
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length) //mesmo que Math.random()*3
+
+    switch (randomNumber) {
+        case 0:
+            computerChoice = "rock"
+            break;
+
+        case 1:
+            computerChoice = "scissors"
+            break
+
+        case 2:
+            computerChoice = "paper"
+            break
+
+    }
+
+    //preenchendo na tela a escolha da máquina
+    computerChoiceDisplay.innerHTML = computerChoice
+
+}
+
+function getResult() {
+
+    // possibilidades
+    if (computerChoice === userChoice) {
+        result = "Empate!"
+    } else if (computerChoice === "rock" && userChoice === "scissors") {
+        result = "Você perdeu!"
+    } else if (computerChoice === "paper" && userChoice === "rock") {
+        result = "Você perdeu!"
+    } else if (computerChoice === "scissors" && userChoice === "paper") {
+        result = "Você perdeu!"
+    } else {
+        result = "Você ganhou!"
+    }
+
+    // colocando resultado no display
+    resultDisplay.innerHTML = result
+}
